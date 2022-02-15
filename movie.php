da85140c752842ef75982be7330c016381d80740
ChriEffe <christianferrazzano93@gmail.com>
2022-02-15 15:22:12 +0100

    stampo a schermo

--- movie.php
+++ movie.php
@@ -50,3 +50,25 @@ $movies = new Movie('Pulp Fiction', 'John Travolta, Samuel L. Jackson, Tim Roth,
 $moviesB = new Movie('Matrix', 'Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving, Gloria Foster', 'Lana Wachowski, Lilly Wachowski');
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li><h1>Title: <?php echo $movies->getTitle() ?></h1></li>
+        <li><h2>Cast: <?php echo $movies->getCast() ?></h2></li>
+        <li><h3>Regista: <?php echo $movies->getMovieDirector() ?></h3></li>
+    </ul>
+    <ul>
+        <li><h1>Title: <?php echo $moviesB->getTitle() ?></h1></li>
+        <li><h2>Cast: <?php echo $moviesB->getCast() ?></h2></li>
+        <li><h3>Regista: <?php echo $moviesB->getMovieDirector() ?></h3></li>
+    </ul>
+</body>
+</html>
