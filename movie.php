3df55bebed474226682d1508ebb39baab87efd36
ChriEffe <christianferrazzano93@gmail.com>
2022-02-15 15:09:12 +0100

    get e set

--- movie.php
+++ movie.php
@@ -1,6 +1,6 @@
 <?php
 
-class movie
+class Movie
 {
     public $title;
     public $runningTime;
@@ -8,6 +8,42 @@ class movie
     public $cast;
     public $plot;
     public $language;
+
+    public function __construct($title, $cast, $language)
+    {
+        $this->title = $title;
+        $this->cast = $cast;
+        $this->language = $language;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setCast($cast)
+    {
+        $this->cast = $cast;
+    }
+    public function getCast()
+    {
+        return $this->cast;
+    }
+
+    public function setLanguage($language)
+    {
+        $this->language = $language;
+    }
+    public function getLanguage()
+    {
+        return $this->language;
+    }
 }
+
+
 
 ?>
